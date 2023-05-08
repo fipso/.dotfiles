fec75e7a8ed683fa1f66287db71031ba90b83e5f
fipso <fipso@users.noreply.github.com>
2023-05-08 22:53:46 +0200

    add basic options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,8 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Example using a list of specs with the default options
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 require("lazy").setup({
 	"nvim-lua/plenary.nvim",
@@ -37,21 +38,51 @@ require("lazy").setup({
 	"hrsh7th/nvim-cmp",
 	"L3MON4D3/LuaSnip",
 	"saadparwaiz1/cmp_luasnip",
+        {
+          "nvim-treesitter/nvim-treesitter",
+          build = ":TSUpdate"
+        },
 	"folke/tokyonight.nvim",
 	"Shatur/neovim-ayu",
 	"junegunn/fzf",
 	"junegunn/fzf.vim",
 	"tpope/vim-fugitive",
-	"tpope/vim-commentary"
+	"tpope/vim-commentary",
+	"nvim-tree/nvim-web-devicons",
+	"nvim-tree/nvim-tree.lua",
+	"folke/trouble.nvim",
 })
 
 require("complete")
 require("lsp")
 
+require("nvim-tree").setup()
+
 vim.wo.number = true
 vim.opt.termguicolors = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.spelllang = 'en-gb'
+vim.o.mouse = 'a'
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.expandtab = true
+vim.o.softtabstop = 2
+vim.o.hidden = true
+vim.o.smartindent = true
+vim.o.cmdheight = 1
+vim.o.signcolumn = 'number'
+vim.o.equalalways = false
+vim.o.shiftwidth = 2
 
-vim.cmd[[colorscheme ayu-light]]
+vim.cmd[[colorscheme ayu-dark]]
 
-vim.keymap.set('n', '<leader>f', '<Cmd>:Files<CR>', {silent = true })
-vim.keymap.set('n', '<leader>l', '<Cmd>lua vim.lsp.buf.formatting()<CR>', {silent = true })
+vim.keymap.set('n', '<leader>f', '<Cmd>:Files<CR>', {silent = true})
+vim.keymap.set('n', '<leader>g', '<Cmd>:Rg<CR>', {silent = true})
+vim.keymap.set('n', '<leader>l', '<Cmd>lua vim.lsp.buf.formatting()<CR>', {silent = true})
+
+vim.keymap.set('n', '<leader>c', '<Cmd>:e ~/.config/nvim/<CR>', {silent = true})
+
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>', {silent = true})
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>', {silent = true})
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>', {silent = true})
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>', {silent = true})
