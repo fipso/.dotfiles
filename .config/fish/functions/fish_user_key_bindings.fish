c9f37a8356af185046b7b30f68ca16159249c17d
fipso <fipso@users.noreply.github.com>
2025-04-18 14:33:40 +0200

    update

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,4 @@
 function fish_user_key_bindings
-        bind --mode insert ctrl-o prevd
-        bind --mode insert ctrl-i nextd
-    
+  bind --mode insert ctrl-o "prevd >/dev/null; commandline -f repaint"
+  bind --mode insert ctrl-i "nextd >/dev/null; commandline -f repaint"
 end
